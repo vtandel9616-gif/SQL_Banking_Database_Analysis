6148aae82efc9185eccf91f40f223bf393785e06
vtandel9616-gif <vtandel9616@gmail.com>
2026-06-16 17:04:08 +0530

    sq1 commit

---
+++ Banking_DB_analysis.sql
@@ -0,0 +1,39 @@
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100),
+    Phone VARCHAR(15),
+    AccountCreationDate DATE
+);
+
+
+CREATE TABLE Accounts (
+    AccountID INT PRIMARY KEY,
+    CustomerID INT,
+    AccountType VARCHAR(20),
+    Balance DECIMAL(10,2),
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID) );
+    
+    CREATE TABLE Transactions (
+    TransactionID INT PRIMARY KEY,
+    AccountID INT,
+    TransactionDate DATE,
+    Amount DECIMAL(10,2),
+    TransactionType VARCHAR(20),
+    FOREIGN KEY (AccountID) REFERENCES Accounts(AccountID) );
+    
+    CREATE TABLE Branches (
+	BranchID INT PRIMARY KEY,
+    BranchName VARCHAR(100),
+    BranchAddress VARCHAR(200),
+    BranchPhone VARCHAR(15)
+);
+
+CREATE TABLE AccountBranches (
+    AccountID INT,
+    BranchID INT,
+    AssignmentDate DATE,
+    FOREIGN KEY (AccountID) REFERENCES Accounts(AccountID),
+    FOREIGN KEY (BranchID) REFERENCES Branches(BranchID)
+);
